9db10be7d8d8c07856502566fe3179825c1af0ec
Jackson L. F. Belizário <jacksonluizzb@gmail.com>
2019-04-06 22:53:18 -0400

    Registro / termos de uso

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,15 +1,13 @@
-import React, { useReducer, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { TextField, Typography } from '@material-ui/core';
+import React, {useReducer, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {Switch, Typography, FormControlLabel, Snackbar, Slide, IconButton} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-// import { LoginSocialMedia } from '../../../../infra/components/LoginSocialMedia';
-// import { Row } from '../../singUp/ui/Utils';
-import { RouterPaths } from '../routes/Routes';
+import {RouterPaths} from '../routes/Routes';
 
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { LockOutlined, LockOpenOutlined } from '@material-ui/icons';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import {LockOutlined, LockOpenOutlined, Close as CloseIcon} from '@material-ui/icons';
+import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
 // import {
 //   fieldEmail,
@@ -22,90 +20,132 @@ import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 //   fieldEmail,
 // };
 
-export const Login = ({ onLogin, createAccount, classes }) => {
-  // const [stateLogin, dispatch] = useReducer(reducer, initialState);
-  //
-  // const { value: password = '', ...propsPassword } = stateLogin.fieldPassword;
-  // const { value: email = '', ...propsEmail } = stateLogin.fieldEmail;
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLogin, setIsLogin] = useState(true);
+export const Login = ({onLogin, createAccount, classes}) => {
+    // const [stateLogin, dispatch] = useReducer(reducer, initialState);
+    //
+    // const { value: password = '', ...propsPassword } = stateLogin.fieldPassword;
+    // const { value: email = '', ...propsEmail } = stateLogin.fieldEmail;
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
+    const [accept, setAccept] = useState(false);
+    const [showSnackBar, setShowSnackBar] = useState(false);
 
-  return (
+    const handleSubmit = () => {
+        if (isLogin) {
+            onLogin(email, password)
+        } else {
+            if (!accept) {
+                return setShowSnackBar(true);
+            }
+            createAccount(name, email, password)
+        }
+    };
 
-      <main className={classes.main}>
-        <CssBaseline />
-        <div className={classes.flex}>
-          <Avatar className={classes.avatar}>
-            <LockOutlined />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-              App Beleza
-          </Typography>
-          <ValidatorForm
-              onSubmit={() => { isLogin ? onLogin(email, password) : createAccount(name, email, password) }}
-              className={classes.form} >
-              {
-                  !isLogin ? (
-                      <TextValidator
-                          fullWidth
-                          label="Nome"
-                          onChange={({ target: { value: newValue } }) => {
-                              setName(newValue);
-                          }}
-                          name="nome"
-                          value={name}
-                          validators={['required']}
-                          errorMessages={['Campo é obrigatório']}
-                      />
-                  ) : ''
-              }
-              <TextValidator
-                  fullWidth
-                  label="Email"
-                  onChange={({ target: { value: newValue } }) => {
-                      setEmail(newValue);
-                  }}
-                  name="email"
-                  value={email}
-                  validators={['required', 'isEmail']}
-                  errorMessages={['Campo é obrigatório', 'email inválido']}
-              />
-              <TextValidator
-                  fullWidth
-                  label="Senha"
-                  type="password"
-                  value={password}
-                  onChange={({ target: { value: newValue } }) => {
-                      setPassword(newValue);
-                  }}
-                  validators={['required']}
-                  errorMessages={['Campo é obrigatório']}
-              />
-            <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                className={classes.button}
-            >
-                <LockOpenOutlined className={classes.lockIcon} />
-                { isLogin ? 'Entrar' : 'Registrar' }
-            </Button>
-              <Button
-                  type="button"
-                  variant="outlined"
-                  className={classes.button}
-                  onClick={() => { setIsLogin(!isLogin) }}
-              >
-                  {
-                      isLogin
-                          ? 'Se ainda não é cadastrado, clique aqui'
-                          : 'Se já é cadastrado, clique aqui'
-                  }
-              </Button>
-          </ValidatorForm>
-        </div>
-      </main>
-  );
+    return (
+
+        <main className={classes.main}>
+            <CssBaseline/>
+            <div className={classes.flex}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlined/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    App Beleza
+                </Typography>
+                <ValidatorForm
+                    onSubmit={handleSubmit}
+                    className={classes.form}>
+                    {
+                        !isLogin ? (
+                            <TextValidator
+                                fullWidth
+                                label="Nome"
+                                onChange={({target: {value}}) => {
+                                    setName(value);
+                                }}
+                                name="nome"
+                                value={name}
+                                validators={['required']}
+                                errorMessages={['Campo é obrigatório']}
+                            />
+                        ) : ''
+                    }
+                    <TextValidator
+                        fullWidth
+                        label="Email"
+                        onChange={({target: {value}}) => {
+                            setEmail(value);
+                        }}
+                        name="email"
+                        value={email}
+                        validators={['required', 'isEmail']}
+                        errorMessages={['Campo é obrigatório', 'email inválido']}
+                    />
+                    <TextValidator
+                        fullWidth
+                        label="Senha"
+                        type="password"
+                        value={password}
+                        onChange={({target: {value}}) => {
+                            setPassword(value);
+                        }}
+                        validators={['required']}
+                        errorMessages={['Campo é obrigatório']}
+                    />
+                    {
+                        !isLogin
+                            ? (<FormControlLabel control={
+                                <Switch color="primary" checked={accept} onChange={({target: {checked}}) => {
+                                    setAccept(checked)
+                                }}/>}
+                                                 label={<div>Aceito os <Link to='#'>Termos de Uso</Link></div>}/>)
+                            : ''
+                    }
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                    >
+                        <LockOpenOutlined className={classes.lockIcon}/>
+                        {isLogin ? 'Entrar' : 'Registrar'}
+                    </Button>
+                    <Button
+                        type="button"
+                        variant="outlined"
+                        className={classes.button}
+                        onClick={() => {
+                            setIsLogin(!isLogin)
+                        }}
+                    >
+                        {
+                            isLogin
+                                ? 'Se ainda não é cadastrado, clique aqui'
+                                : 'Se já é cadastrado, clique aqui'
+                        }
+                    </Button>
+                </ValidatorForm>
+                <Snackbar
+                    open={showSnackBar}
+                    TransitionComponent={(props) => <Slide {...props} direction="up"/>}
+                    ContentProps={{
+                        'aria-describedby': 'message-id',
+                    }}
+                    message={<span id="message-id">Você deve aceitar os Termos de Uso</span>}
+                    action={<IconButton
+                        key="close"
+                        aria-label="Close"
+                        color="inherit"
+                        onClick={() => {
+                            setShowSnackBar(false)
+                        }}
+                    >
+                        <CloseIcon/>
+                    </IconButton>}
+                />
+            </div>
+        </main>
+    );
 };
